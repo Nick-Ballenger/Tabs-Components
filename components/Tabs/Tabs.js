95560c8b0d25b77bdfaec32a39fc48f6da8c8822
Nick-Ballenger <Nick-Ballenger>
2019-03-07 15:55:00 -0600

    fixed some things to work a little better. Tested out more with console logs

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -4,10 +4,10 @@ class TabLink {
     
     // Get the custom data attribute on the Link
     this.data = this.element.dataset.tab;
-    
+    console.log(this.data);
     // Using the custom data attribute get the associated Item element
     this.itemElement = document.querySelector(`.tabs-item[data-tab = "${this.data}"]`);
-    
+ 
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
     
@@ -22,7 +22,8 @@ class TabLink {
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     Array.from(links).forEach(link =>{
       link.classList.remove('tabs-link-selected');
-      //console.log(links);
+      
+      
     });
 
     // Add a class named "tabs-link-selected" to this link
@@ -47,7 +48,7 @@ class TabItem {
       item.classList.remove('tabs-item-selected');
     })
     // Add a class named "tabs-item-selected" to this element 
-    this.element.classList.add('tabs-item-selected')
+    this.element.classList.toggle('tabs-item-selected')
   }
 }
 
